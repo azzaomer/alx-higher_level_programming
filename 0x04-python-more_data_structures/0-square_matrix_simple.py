b98131558afd541bd848c2496e11ab32ef227b38
azzaomer <azzaomer70@gmail.com>
2024-03-25 21:19:53 -0700

    computes the square value of all integers of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[elem**2 for elem in inner] for inner in matrix]
-
+    new_matrix = [[x ** 2 for x in row] for row in matrix]
     return new_matrix
