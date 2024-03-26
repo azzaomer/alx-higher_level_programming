fc6b602b79406d99d6384ff9e2863be9d014ed50
azzaomer <azzaomer70@gmail.com>
2024-03-26 15:36:24 -0700

    roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -11,7 +11,7 @@ def roman_to_int(roman_string):
     res = 0
     for j in range(len(roman_string)):
         if j > 0 and dic[roman_string[j]] > dic[roman_string[j - 1]]:
-            result += dic[roman_string[j]] - 2 * dic[roman_string[j - 1]]
+            res += dic[roman_string[j]] - 2 * dic[roman_string[j - 1]]
         else:
             res += dic[roman_string[j]]
     return res
