63257acb961fc9f3ee893819da6fef35a89628e1
azzaomer <azzaomer70@gmail.com>
2024-04-08 13:10:09 -0700

    5. Printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -22,18 +22,19 @@ class Square:
         return self.__size ** 2
     
     def my_print(self):
-        for i in self.__size/2:
-            for j in self.__size/2:
-                print("#")
-    @propert
+        for i in range(self.size):
+            print("#" * self.size)
+        else:
+            print()
+    @property
     def size(self):
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
