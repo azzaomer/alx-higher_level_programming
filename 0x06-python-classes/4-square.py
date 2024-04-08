37d6d22b3e694491ab5c122d3ab0bbbf2d79350b
azzaomer <azzaomer70@gmail.com>
2024-04-07 22:45:19 -0700

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,18 +17,18 @@ class Square:
         """
         self.__size = size
 
-    @propert 
-    def size(self):
-        return self.__size
-    @size.setter
-    def size(self, value):
-        if size.isdigit():
-            self.__size = value
-        else:
-            raise TypeError("size must be an integer")
-        if self.__size >= 0:
-            self.__size =value
-        else:
-            raise ValueError("size must be >= 0")
     def area(self):
         return self.__size ** 2
+
+    @propert
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
