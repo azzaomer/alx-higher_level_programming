5088c8480398e0d476c63b556ea166b62d467ce7
azzaomer <azzaomer70@gmail.com>
2024-04-11 16:23:47 -0700

    4. Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""function that prints a text with 2 new lines after each,of these characters: . ? and :
+"""function that prints a text with 2 new lines after each,
+of these characters: . ? and :
 Attributes:
     text_indentation: function that prints a text
 """
