8e4545a8c70068471fc5599aef91db0eadbc4c99
azzaomer <azzaomer70@gmail.com>
2024-04-11 16:05:33 -0700

    3. Print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,6 +3,8 @@
 
 Attributes: print_squer: function that prints my name.
 """
+
+
 def print_square(size):
     """Print square
 
@@ -20,6 +22,6 @@ def print_square(size):
         raise ValueError('size must be >= 0')
     if isinstance(size, float) and (size < 0):
         raise TypeError('size must be an integer')
-    
+
     for i in range(size):
         print("#" * size)
