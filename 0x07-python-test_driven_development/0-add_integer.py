d1b58b287ca36e61f6c771b8408b8bc5d545fb3a
azzaomer <azzaomer70@gmail.com>
2024-04-11 03:32:14 -0700

    0. Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """Adds two integer and/or float values.
+
+    Args:
+        a (int): First value
+        b (int, optional): Second value. Defaults to 98.
+
+    Raises:
+        TypeError: If a and b are not integers or floats.
+
+    Returns:
+        int: Sum of a and b.
+    """
     # Check if a and b are integers or floats
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):       
-        raise TypeError("a must be an integer or b must be an integer")
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
 
     #Cast a and b to integers if they are floats
-    a = int(a)
-    b = int(b)
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
 
     return a+b
 
