a48ac984a052cf2e915cc1547d43412f5f9e4543
azzaomer <azzaomer70@gmail.com>
2024-04-17 18:47:28 -0700

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Module containing the function read_file"""
 
 
 def read_file(filename=""):
+     """Reads a file and prints to stdout.
+
+    Args:
+        filename (str, optional): name of file to read. Defaults to "".
+    """
     with open(filename, "r", encoding="UTF-8") as myfile:
         print(myfile.read(), end="")
