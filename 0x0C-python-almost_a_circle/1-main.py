5009e4b50965ceba73b4839b4f62925811863b75
azzaomer <azzaomer70@gmail.com>
2024-04-24 07:54:40 +0300

    Rectangle

---
+++ 0x0C-python-almost_a_circle/1-main.py
@@ -0,0 +1,15 @@
+#!C:\Users\OP\AppData\Local\Programs\Python\Python312\python.exe
+#!/usr/bin/python3
+""" 1-main """
+from models.rectangle import Rectangle
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(10, 2)
+    print(r1.id)
+
+    r2 = Rectangle(2, 10)
+    print(r2.id)
+
+    r3 = Rectangle(10, 2, 0, 0, 12)
+    print(r3.id)
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,30 @@
+#!C:\Users\OP\AppData\Local\Programs\Python\Python312\python.exe
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Class that defines properties of Rectangle.
+
+     Attributes:
+        width (int): width of rectangle.
+        height (int): height of rectangle.
+        x (int): x.
+        y (int): y.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Creates new instances of rectangle.
+
+        Args:
+            width (int): width of rectangle.
+            height (int): height of rectangle.
+            x (int, optional): x. Defaults to 0.
+            y (int, optional): y. Defaults to 0.
+            id (int, optional): Identity number of rectangle. Defaults to None.
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
