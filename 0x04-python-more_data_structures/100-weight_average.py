bdde65012373035443bed65d0c20429d39961fa6
azzaomer <azzaomer70@gmail.com>
2024-05-10 05:48:06 -0700

    average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    def sum_scores(scores):
-        list_scores = list(map(lambda a: a[0] * a[1], scores))
-        return sum(list_scores)
+def sum_scores(scores):
+    list_scores = list(map(lambda a: a[0] * a[1], scores))
+    return sum(list_scores)
     
-    def sum_weights(scores):
-        list_weights = list(map(lambda a: a[1], scores))
-        return sum(list_weights)
+def sum_weights(scores):
+    list_weights = list(map(lambda a: a[1], scores))
+    return sum(list_weights)
    
-    def weight_average(my_list=[]):
-        if lien(my_list) == 0:
-            return 0
-        else:
-            average = sum_scores(my_list) / sum_weights(my_list)
-        return average
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    else:
+        average = sum_scores(my_list) / sum_weights(my_list)
+    return average
