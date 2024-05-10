6eb32e27143a9b12ca313728f800ca9b708a385d
azzaomer <azzaomer70@gmail.com>
2024-05-10 05:49:44 -0700

    average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,11 +2,13 @@
 def sum_scores(scores):
     list_scores = list(map(lambda a: a[0] * a[1], scores))
     return sum(list_scores)
-    
+
+
 def sum_weights(scores):
     list_weights = list(map(lambda a: a[1], scores))
     return sum(list_weights)
-   
+
+
 def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
