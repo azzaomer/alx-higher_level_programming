466e7c7c4300c5cb5906f3639e4d01bae17a49c1
azzaomer <azzaomer70@gmail.com>
2024-05-22 06:46:38 -0700

    create_read_user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create user
-CREATE USER IF NOT EXISTS 'user_0d_2@localhost' IDENTIFIED BY 'user_0d_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_pwd';
 -- grant priv
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2@localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
