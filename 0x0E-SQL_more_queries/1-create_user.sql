3e8736a1efe703553f242232f16ff93cbe3721e2
azzaomer <azzaomer70@gmail.com>
2024-05-22 06:48:25 -0700

    create_read_user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- script that creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant privs
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 -- flush
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create user
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_pwd';
--- grant priv
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- grant privs
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
