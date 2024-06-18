d9f34dc7220d42dc5e38a235048d082bbbd1a937
azzaomer <azzaomer70@gmail.com>
2024-06-19 00:35:00 +0300

    args

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const process = require('process');
+var args = process.argv;
+if (args.length = 0){
+    console.log('No argument');
+} else if (args.length = 1 ){
+    console.log('Argument found');
+} else {
+    console.log('Argument found');
+}
