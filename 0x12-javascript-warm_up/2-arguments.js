7ad03ae4e20675488789ae14a31007227254b540
azzaomer <azzaomer70@gmail.com>
2024-06-19 00:37:27 +0300

    args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-const process = require('process');
 var args = process.argv;
-if (args.length = 0){
+if (args.length === 2) {
     console.log('No argument');
-} else if (args.length = 1 ){
+} else if (args.length === 3 ) {
     console.log('Argument found');
 } else {
     console.log('Argument found');
