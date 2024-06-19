30f0d2a055538e046b5ab48c3035b3ccc13cf29b
azzaomer <azzaomer70@gmail.com>
2024-06-18 18:56:07 -0700

    while

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 
 const lang = 'C is fun';
-if (isNaN(process.argv[2])) {
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < parseInt(process.aegv[2]); i++) {
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
     console.log(lang);
+    i++;
   }
 }
