de716c70cc89746166dfad0b1ed24e4ddea1bbbf
azzaomer <azzaomer70@gmail.com>
2024-06-20 00:43:54 +0300

    number of occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+//  a function that returns the number of occurrences in a lis
 exports.nbOccurences = function (list, searchElement) {
   return list.reduce((count, current) => current === searchElement
     ? count + 1
