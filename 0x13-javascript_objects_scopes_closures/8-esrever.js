5aad73fec23dedc8e0d8bd19e7c504abca57227a
azzaomer <azzaomer70@gmail.com>
2024-06-20 00:45:31 +0300

    reverse array

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+// a function that returns the reversed version of a list:
 exports.esrever = function (list) {
   return list.reduceRight(function (array, current) {
     array.push(current);
