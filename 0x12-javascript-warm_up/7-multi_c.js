81ddef14a45338ff1d3dfd3ece83a0811ca056ef
azzaomer <azzaomer70@gmail.com>
2024-06-18 17:56:14 -0700

    for loop

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,6 +5,6 @@ if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < parseInt(process.aegv[2]); i++) {
-  console.log(lang);
-}
+    console.log(lang);
+  }
 }
