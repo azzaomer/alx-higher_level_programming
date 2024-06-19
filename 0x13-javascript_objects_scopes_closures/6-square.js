ef272d491b2cb9653857e71967e87e87a58867f5
azzaomer <azzaomer70@gmail.com>
2024-06-20 00:43:05 +0300

    square inherits rectangle

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+// a class Square that defines a square and inherits from Square of 5-square.js
 module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
