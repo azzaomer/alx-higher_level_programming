a1fbd892f42e4074a43108a707fcda7cde0a78d8
azzaomer <azzaomer70@gmail.com>
2024-06-18 19:12:14 -0700

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+//a script that prints a square
+
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const size = Number(process.argv[2]);
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
