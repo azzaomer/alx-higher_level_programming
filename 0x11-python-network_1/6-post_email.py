732edcd15a8784cef5242ad2ad7794cd25472bec
azzaomer <azzaomer70@gmail.com>
2024-07-05 17:27:42 -0700

    post email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL
+and an email address, sends a POST request
+to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+
+import requests
+from sys import argv
+
+if __name == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    rq = requests.get(url, data=value)
+
+    print(rq.text)
