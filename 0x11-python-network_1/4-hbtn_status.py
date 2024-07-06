be505fc2c7948189b60275d0562cc3fa581f2c11
azzaomer <azzaomer70@gmail.com>
2024-07-05 17:16:42 -0700

    requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+if __name__ == "__main__":
+    rq = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(rq.text)))
+    print("\t- content: {}".format(rq.text))
+
