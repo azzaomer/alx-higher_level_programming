e3a10ebefa5b1f809254998b1ee530a8ff22e762
azzaomer <48832222+azzaomer@users.noreply.github.com>
2024-08-30 17:44:39 +0300

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 //  script that prints x times “C is fun”
 // x is the first argument of the script
-function factorial (n) {
-    if (n < 0) {
-        return (-1);
-    }
-    if (n === 0 || isNaN(n)) {
-        return (1);
-    }
-    return (n * factorial(n - 1));
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
-    console.log(factorial(Number(process.argv[2])));
