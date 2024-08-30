f021ae7365d55c0c2b1a87c96879cfd741fbe666
azzaomer <48832222+azzaomer@users.noreply.github.com>
2024-08-30 17:39:58 +0300

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-
+// a script that prints 3 lines by using an array of string and a loop
 const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < 3; i++) {
   console.log(lang[i]);
