736ec298a770853f4a6613068242e424d0df3bc3
azzaomer <azzaomer70@gmail.com>
2024-09-01 14:13:42 +0300

    js max

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,24 +1,18 @@
 #!/usr/bin/node
-// script that searches the second biggest
-// integer in the list of arguments
+// Script that searches for the second biggest integer
+// in the list of arguments
+
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  let max = process.argv[2];
-  let maxTwo = process.argv[3];
-  for (let i = 3; i < process.argv.length; i++) {
-    if (process.argv[i] > max) {
-      max = process.argv[i];
-    }
-  }
-  console.log(`The first max = ${max}`)
-  console.log(`Initaial max 2 = ${maxTwo}`)
-  for (let j = 2; j < process.argv.length; j++) {
-    console.log(`argument ${j}: ` +  process.argv[j])
-    if (process.argv[j] > maxTwo && process.argv[j] !== max) {
-      maxTwo = process.argv[j];
-      console.log(`the second bigest: ` + process.argv[j])
-    }
-  }
+  // Convert the arguments to integers and store them in an array
+  const args = process.argv.slice(2).map(Number);
+
+  // Sort the array in descending order
+  args.sort((a, b) => b - a);
+
+  // The second biggest number will be the second item in the sorted array
+  const maxTwo = args[1];
+
   console.log(maxTwo);
 }
