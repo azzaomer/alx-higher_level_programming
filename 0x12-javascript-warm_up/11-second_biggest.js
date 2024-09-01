fd1cda9e97e618d0f712f0fbdf2e9c623d2d85cf
azzaomer <azzaomer70@gmail.com>
2024-09-01 13:48:24 +0300

    js max

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,14 +4,14 @@
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  let max = process.argv[1];
-  let maxTwo = process.argv[2];
-  for (let i = 0; i < process.argv.length; i++) {
+  let max = process.argv[2];
+  let maxTwo = process.argv[3];
+  for (let i = 2; i < process.argv.length; i++) {
     if (process.argv[i] > max) {
       max = process.argv[i];
     }
   }
-  for (let j = 0; j < process.argv.length; j++) {
+  for (let j = 2; j < process.argv.length; j++) {
     if (process.argv[j] > maxTwo && process.argv[j] !== max) {
       maxTwo = process.argv[j];
     }
