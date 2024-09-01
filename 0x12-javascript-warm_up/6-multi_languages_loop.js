18e5261b5d3f676f2811b5a35ded5782f53c7309
azzaomer <azzaomer70@gmail.com>
2024-09-01 13:40:07 +0300

    js for

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // a script that prints 3 lines by using an array of string and a loop
-const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < 3; i++) {
   console.log(lang[i]);
 }
